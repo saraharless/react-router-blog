d1fc0f67437a91145ada1ec0bd62669c02c5ccdf
saraharless <sara.e.harless@gmail.com>
2017-10-01 15:09:43 -0500

    folder structure, import browserruoute, route and switch into index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
