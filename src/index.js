c2da9f27e308715312c20e411bd89210254d43b2
saraharless <sara.e.harless@gmail.com>
2017-10-01 15:13:46 -0500

    add baselayout component and imports to index.js

--- src/index.js
+++ src/index.js
@@ -4,5 +4,21 @@ import './styles/index.css';
 import registerServiceWorker from './registerServiceWorker';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Home from './components/App.js';
+import Create from './components/CreatePost.js';
+import ShowAll from './components/PostList.js';
+
+ReactDOM.render(
+<BrowserRouter>
+<BaseLayout>
+  <Switch>
+    <Route exact path='/' component={Home} />
+    <Route path='/create' component={Create} />
+    <Route path='/showall' component={ShowAll} />
+  </Switch>
+</BaseLayout>
+</BrowserRouter>
+
+,document.getElementById('root')
+);
 registerServiceWorker();
